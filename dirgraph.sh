477cd69dd4e7a016e30f7beff8fcf82e144c6464
alukard <aetherofchao@gmail.com>
2020-03-22 17:36:07 +0100

    graph added

--- dirgraph.sh
+++ dirgraph.sh
@@ -17,31 +17,22 @@ updateCount() {
 	#echo $1 , $2
 	if   [[ $1 -le 100 ]] 		; then 	#0 100 B
 		ArrayCount[0]=`expr ${ArrayCount[0]} + 1`
-		echo $1
 	elif [[ $1 -le 1024 ]] 		; then 	#1 1KiB
 		ArrayCount[1]=`expr ${ArrayCount[1]} + 1`
-		echo $1
 	elif [[ $1 -le 10240 ]]		; then 	#2 10KiB
 		ArrayCount[2]=`expr ${ArrayCount[2]} + 1`
-		echo $1
 	elif [[ $1 -le 102400 ]]	; then 	#3 100KiB
 		ArrayCount[3]=`expr ${ArrayCount[3]} + 1`
-		echo $1
 	elif [[ $1 -le 1048576 ]]	; then 	#4 1MiB
 		ArrayCount[4]=`expr ${ArrayCount[4]} + 1`
-		echo $1
 	elif [[ $1 -le 10485760 ]]	; then 	#5 10MiB
 		ArrayCount[5]=`expr ${ArrayCount[5]} + 1`
-		echo $1
 	elif [[ $1 -le 104857600 ]]	; then 	#6 100MiB
 		ArrayCount[6]=`expr ${ArrayCount[6]} + 1`
-		echo $1
 	elif [[ $1 -le 1073741824 ]]; then 	#7 1GiB
 		ArrayCount[7]=`expr ${ArrayCount[7]} + 1`
-		echo $1
 	else 								#8 >1GiB
 		ArrayCount[8]=`expr ${ArrayCount[8]} + 1`
-		echo $1
 	fi
 }
 
@@ -62,7 +53,18 @@ MainLoop() {
 }
 
 drawLine() {
-	echo $1
+	#echo $1
+	count=$1
+	if [[ count -gt $(tput cols)-12 ]] ;then
+		count=`expr $(tput cols) - 12`
+	fi 
+
+	#echo $count $(tput cols)
+
+	while [[ $count -gt 0 ]]; do
+		echo -n "#"
+		count=`expr $count - 1`
+	done
 }
 
 
@@ -78,12 +80,12 @@ echo "Directories: $DirCount"
 echo "All files: $FileCount"
 echo "File size histogram:"
 
-echo "  <100 B  :" ${ArrayCount[0]}
-echo "  <1 KiB  :" ${ArrayCount[1]}
-echo "  <10 KiB :" ${ArrayCount[2]}
-echo "  <100 KiB:" ${ArrayCount[3]}
-echo "  <1 MiB  :" ${ArrayCount[4]}
-echo "  <10 MiB :" ${ArrayCount[5]}
-echo "  <100 MiB:" ${ArrayCount[6]}
-echo "  <1 GiB  :" ${ArrayCount[7]}
-echo "  >=1 GiB :" ${ArrayCount[8]}
+echo "  <100 B  :" $(drawLine ${ArrayCount[0]})
+echo "  <1 KiB  :" $(drawLine ${ArrayCount[1]})
+echo "  <10 KiB :" $(drawLine ${ArrayCount[2]})
+echo "  <100 KiB:" $(drawLine ${ArrayCount[3]})
+echo "  <1 MiB  :" $(drawLine ${ArrayCount[4]})
+echo "  <10 MiB :" $(drawLine ${ArrayCount[5]})
+echo "  <100 MiB:" $(drawLine ${ArrayCount[6]})
+echo "  <1 GiB  :" $(drawLine ${ArrayCount[7]})
+echo "  >=1 GiB :" $(drawLine ${ArrayCount[8]})
